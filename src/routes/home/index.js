ee742dc88b1430a2abd65543a1957f46c90fb384
sergeysatori <38408406+sergeysatori@users.noreply.github.com>
2019-02-14 17:17:29 -0300

    title

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,16 +8,9 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={`${style.home} page`}>
-				<h1>Home route</h1>
-				<Card>
-					<div class={style.cardHeader}>
-						<h2 class=" mdc-typography--title">Home card</h2>
-						<div class=" mdc-typography--caption">Welcome to home route</div>
-					</div>
-					<div class={style.cardBody}>
-						<iframe style={{height: '100%', width:  '100%'}} src={'/embed/avatar-demo/webgl_loader_obj_mtl.html'}></iframe>
-					</div>					
-				</Card>
+
+				<iframe style={{height: '100%', width:  '100%'}} src={'/embed/avatar-demo/webgl_loader_obj_mtl.html'}></iframe>
+
 			</div>
 		);
 	}
