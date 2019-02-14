057cef8f63ce83d199909e6785ac75326a52cd13
sergeysatori <38408406+sergeysatori@users.noreply.github.com>
2019-02-14 19:47:52 -0300

    iframe test

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,14 +3,21 @@ import Card from 'preact-material-components/Card';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
 import style from './style';
+import Iframe from 'react-iframe';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div class={`${style.home} page`}>
 
-				<iframe style={{height: '100%', width:  '100%'}} src={'embed/avatar-demo/webgl_loader_obj_mtl.html'}></iframe>
-
+					<Iframe url='embed/avatar-demo/webgl_loader_obj_mtl.html'
+	        width="100%"
+	        height="100%"
+	        id="avatarEmbed"
+	        className="iFrame"
+	        display="initial"
+	        position="relative"
+	        allowFullScreen/>
 			</div>
 		);
 	}
